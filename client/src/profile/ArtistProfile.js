113805b58a134ce6ec396215fe878eef1120e2ce
Michael Danaher <mikel.d@live.ie>
2022-11-21 21:57:54 -0500

    Started working on Artist Profile. Cleaned up Cards

--- client/src/profile/ArtistProfile.js
+++ client/src/profile/ArtistProfile.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react'
 import {useLocation} from "react-router-dom";
-import {Box, Grid, Tab, Tabs, Typography} from "@material-ui/core"
+import {Box, Grid, Tab, Typography} from "@material-ui/core"
+import TabPanel from "@mui/lab/TabPanel";
 import {AxiosRequest} from "../hooks/AxiosRequest";
 import Header from "./Header";
 import {Divider} from "@mui/material";
+import {TabContext, TabList} from "@mui/lab";
 
 const ArtistProfile = () => {
     const location = useLocation()
     const name = location.state.s
     const {artists, error, loaded} = AxiosRequest(`/artist`, 'get', null, {s: name})
-    const [tabIndex, setTabIndex] = useState(0);
+    const [tabIndex, setTabIndex] = useState("1");
+
 
     const handleTabChange = (event, newTabIndex) => {
         setTabIndex(newTabIndex)
@@ -21,46 +24,30 @@ const ArtistProfile = () => {
         ) : (
             <>
                 <Header artist={artists}/>
-                <Grid item xs={12} style={{display: "flex", width: '100%', flexDirection: "column", justifyContent: "center"}}>
+                <Grid item xs={12}
+                      style={{display: "flex", width: '100%', flexDirection: "column", justifyContent: "center"}}>
                     <Box sx={{width: '100%'}}>
-                        <Tabs value={tabIndex} onChange={handleTabChange} centered>
-                            <Tab label="About"></Tab>
-                            <Tab label="Media"></Tab>
-                            <Tab label="Tab 3"></Tab>
-                        </Tabs>
-                    </Box>
-                    <Box sx={{ padding: 2 }}>
-                        {tabIndex === 0 && (
-                            <Box>
-                                <Divider></Divider>
-                                <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-                                    in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-                            </Box>
-                        )}
-                        {tabIndex === 1 && (
-                            <Box>
-                                <Typography>The second tab</Typography>
-                            </Box>
-                        )}
-                        {tabIndex === 2 && (
-                            <Box>
-                                <Typography>The third tab</Typography>
-                            </Box>
-                        )}
+                        <TabContext value={tabIndex}>
+                            <TabList onChange={handleTabChange} centered>
+                                <Tab label="About" value='1'/>
+                                <Tab label="Media" value='2'></Tab>
+                                <Tab label="Tab 3" value='3'></Tab>
+                            </TabList>
+                            <Divider></Divider>
+                    <TabPanel value="1">
+                        <Typography variant={"body1"}>{artists.bio}</Typography></TabPanel>
+                    <TabPanel value="2"></TabPanel>
+                    <TabPanel value="3">Web Development</TabPanel>
+                        </TabContext>
                     </Box>
                 </Grid>
             </>
 
-        )
+    )
     }
     return (
         <span>Loading...</span>
     )
-}
+    }
 
-export default ArtistProfile
+    export default ArtistProfile
